0f915fcf867c57d94fb2c09c972fe27e78a49004
bourbon6312 <513806156@qq.com>
2023-01-31 00:41:40 +0800

    课程计划接口

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -1,19 +1,16 @@
 package com.xuecheng.base.exception;
-
-/**
- * @author Mr.M
- * @version 1.0
+ /**
  * @description 校验分组
+ * @author Mr.M
  * @date 2022/9/8 15:05
+ * @version 1.0
  */
 public class ValidationGroups {
 
-    //用于添加校验
-    public interface Insert {};
-
-    //用于修改校验
-    public interface Update {};
-
-    public interface Delete {};
+ //用于添加校验
+ public interface Inster{};
+ //用于修改校验
+ public interface Update{};
+ public interface Delete{};
 
 }
