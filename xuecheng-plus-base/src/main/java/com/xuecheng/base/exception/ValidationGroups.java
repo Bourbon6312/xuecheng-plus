7ac46f5c5ee95cde41a14060f675458bdf00e707
bourbon6312 <513806156@qq.com>
2023-01-30 16:36:58 +0800

    添加了全局异常处理

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,19 @@
+package com.xuecheng.base.exception;
+
+/**
+ * @author Mr.M
+ * @version 1.0
+ * @description 校验分组
+ * @date 2022/9/8 15:05
+ */
+public class ValidationGroups {
+
+    //用于添加校验
+    public interface Insert {};
+
+    //用于修改校验
+    public interface Update {};
+
+    public interface Delete {};
+
+}
