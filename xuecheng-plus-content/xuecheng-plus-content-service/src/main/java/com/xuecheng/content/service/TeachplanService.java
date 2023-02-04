a2d8da27a95a4ecfc6aeecc3b6a0a0c8ddbc57db
bourbon6312 <513806156@qq.com>
2023-02-04 17:03:24 +0800

    绑定媒资接口

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/TeachplanService.java
@@ -1,27 +1,37 @@
 package com.xuecheng.content.service;
 
+import com.xuecheng.content.model.dto.BindTeachplanMediaDto;
 import com.xuecheng.content.model.dto.SaveTeachplanDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.TeachplanMedia;
 
 import java.util.List;
 
 /**
- * @description TODO
  * @author Mr.M
- * @date 2022/10/10 14:51
  * @version 1.0
+ * @description TODO
+ * @date 2022/10/10 14:51
  */
 public interface TeachplanService {
 
- public List<TeachplanDto> findTeachplayTree(Long courseId);
+    public List<TeachplanDto> findTeachplayTree(Long courseId);
 
- /**
-  * @description 保存课程计划(新增/修改)
-  * @param dto
-  * @return void
-  * @author Mr.M
-  * @date 2022/10/10 15:07
- */
- public void saveTeachplan(SaveTeachplanDto dto);
+    /**
+     * @param dto
+     * @return void
+     * @description 保存课程计划(新增 / 修改)
+     * @author Mr.M
+     * @date 2022/10/10 15:07
+     */
+    public void saveTeachplan(SaveTeachplanDto dto);
+
+    /**
+     * 教学计划绑定媒资
+     *
+     * @param bindTeachplanMediaDto
+     * @return
+     */
+    public TeachplanMedia associationMedia(BindTeachplanMediaDto bindTeachplanMediaDto);
 
 }
