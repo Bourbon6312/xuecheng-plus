123eef384ad6c99b2cda1f64aabc6667924c2ad8
bourbon6312 <513806156@qq.com>
2023-02-13 02:28:39 +0800

    我的课程表

--- xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/MyCourseTablesService.java
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/xuecheng/learning/service/MyCourseTablesService.java
@@ -1,5 +1,8 @@
 package com.xuecheng.learning.service;
 
+import com.xuecheng.base.model.PageResult;
+import com.xuecheng.learning.model.dto.MyCourseTableItemDto;
+import com.xuecheng.learning.model.dto.MyCourseTableParams;
 import com.xuecheng.learning.model.dto.XcChooseCourseDto;
 import com.xuecheng.learning.model.dto.XcCourseTablesDto;
 import com.xuecheng.learning.model.po.XcChooseCourse;
@@ -34,4 +37,6 @@ public interface MyCourseTablesService {
     public XcCourseTablesDto getLeanringStatus(String userId, Long courseId);
 
     public XcCourseTables addCourseTabls(XcChooseCourse xcChooseCourse);
+
+    public PageResult<XcCourseTables> mycourestabls(MyCourseTableParams params);
 }
